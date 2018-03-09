198abacacbd6cb839e40aadba9c67771f9c3ec3e
Rauno Piibor <31948944+raunopiibor123@users.noreply.github.com>
2018-03-09 10:47:32 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,164 @@
+/* TYPER */
+var counter=0;
+var length=0;
+const TYPER = function () {
+  if (TYPER.instance_) {
+    return TYPER.instance_
+  }
+  TYPER.instance_ = this
+
+  this.WIDTH = window.innerWidth
+  this.HEIGHT = window.innerHeight
+  this.canvas = null
+  this.ctx = null
+
+  this.words = []
+  this.word = null
+  this.wordMinLength = 5
+  this.guessedWords = 0
+  this.generatedWordLength=0 
+  this.length = length
+  this.counter = counter
+  this.splitScore = 0
+  this.score = 0
+  
+  this.init()
+}
+
+
+
+function init () {
+  scoreContainer = document.querySelector('score')
+  console.log(scoreContainer)
+}
+
+
+
+window.TYPER = TYPER
+
+TYPER.prototype = {
+  init: function () {
+    this.canvas = document.getElementsByTagName('canvas')[0]
+    this.ctx = this.canvas.getContext('2d')
+
+    this.canvas.style.width = this.WIDTH + 'px'
+    this.canvas.style.height = this.HEIGHT + 'px'
+
+    this.canvas.width = this.WIDTH * 2
+    this.canvas.height = this.HEIGHT * 2
+
+    this.loadWords()
+  },
+
+  loadWords: function () {
+    const xmlhttp = new XMLHttpRequest()
+
+    xmlhttp.onreadystatechange = function () {
+      if (xmlhttp.readyState === 4 && (xmlhttp.status === 200 || xmlhttp.status === 0)) {
+        const response = xmlhttp.responseText
+        const wordsFromFile = response.split('\n')
+
+        typer.words = structureArrayByWordLength(wordsFromFile)
+
+        typer.start()
+      }
+    }
+
+    xmlhttp.open('GET', './lemmad2013.txt', true)
+    xmlhttp.send()
+  },
+
+  start: function () {
+    this.generateWord()
+    this.word.Draw()
+
+    window.addEventListener('keypress', this.keyPressed.bind(this))
+  },
+
+  generateWord: function () {
+    const generatedWordLength = this.wordMinLength + parseInt(this.guessedWords / 5)
+    const randomIndex = (Math.random() * (this.words[generatedWordLength].length - 1)).toFixed()
+    const wordFromArray = this.words[generatedWordLength][randomIndex]
+    this.word = new Word(wordFromArray, this.canvas, this.ctx)
+	length = generatedWordLength
+	counter = this.guessedWords
+	const splitScore = generatedWordLength * this.guessedWords
+	console.log(splitScore)
+	this.score = this.score + splitScore
+	console.log(this.score)
+	
+	
+	document.getElementById("Words").innerHTML =
+	"Õigesti arvatud sõnu:  " + counter + ".";  
+	document.getElementById("wordLenght").innerHTML =
+	"Sõna pikkus:  " + length+ ".";
+	document.getElementById("streak").innerHTML =
+	"Skoor:  " + this.score + ".";  
+
+  },
+ 
+  keyPressed: function (event) {
+    const letter = String.fromCharCode(event.which)
+
+    if (letter === this.word.left.charAt(0)) {
+      this.word.removeFirstLetter()
+
+      if (this.word.left.length === 0) {
+        this.guessedWords += 1
+
+		
+        this.generateWord()
+      }
+
+      this.word.Draw()
+    }
+  }
+}
+
+
+
+/* WORD */
+const Word = function (word, canvas, ctx) {
+  this.word = word
+  this.left = this.word
+  this.canvas = canvas
+  this.ctx = ctx
+}
+
+Word.prototype = {
+  Draw: function () {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+
+    this.ctx.textAlign = 'center'
+    this.ctx.font = '140px Courier'
+    this.ctx.fillText(this.left, this.canvas.width / 2, this.canvas.height / 2)
+  },
+
+  removeFirstLetter: function () {
+    this.left = this.left.slice(1)
+  }
+}
+
+
+
+
+
+/* HELPERS */
+function structureArrayByWordLength (words) {
+  let tempArray = []
+
+  for (let i = 0; i < words.length; i++) {
+    const wordLength = words[i].length
+    if (tempArray[wordLength] === undefined)tempArray[wordLength] = []
+
+    tempArray[wordLength].push(words[i])
+
+  }
+
+  return tempArray
+}
+
+window.onload = function () {
+  const typer = new TYPER()
+  window.typer = typer
+}
